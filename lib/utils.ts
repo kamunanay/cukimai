0894f35d26318536808a7a7a40de57adf1b9ba71
Anonim <kadal.rigel@gmail.com>
2026-07-06 15:45:29 +0700

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,16 +8,3 @@ export function cn(...inputs: ClassValue[]) {
 export function formatNumber(num: number): string {
   return new Intl.NumberFormat('id-ID').format(num);
 }
-
-export function formatPrice(price: number, currency: string = 'USD'): string {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: currency,
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }).format(price);
-}
-
-export function formatChange(change: number): string {
-  return `${change >= 0 ? '+' : ''}${change.toFixed(2)}%`;
-}
