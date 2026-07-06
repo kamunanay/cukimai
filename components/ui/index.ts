a457dafeffc382d925ae6134ded13dc342696820
Anonim <kadal.rigel@gmail.com>
2026-07-06 19:00:25 +0700

    Create index.ts

---
+++ components/ui/index.ts
@@ -0,0 +1,8 @@
+export { default as LivePriceCard } from './live-price-card';
+export { default as CurrencyConverter } from './currency-converter';
+export { default as MarketStatus } from './market-status';
+export { default as Heatmap } from './heatmap';
+export { default as TopMovers } from './top-movers';
+export { default as Skeleton } from './skeleton';
+export { default as Footer } from './footer';
+export { default as Navbar } from './navbar';
