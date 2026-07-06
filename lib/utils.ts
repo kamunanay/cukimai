1c268b39bae9824a6a85f887593e83111a60f038
Anonim <kadal.rigel@gmail.com>
2026-07-06 15:19:30 +0700

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,23 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatNumber(num: number): string {
+  return new Intl.NumberFormat('id-ID').format(num);
+}
+
+export function formatPrice(price: number, currency: string = 'USD'): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(price);
+}
+
+export function formatChange(change: number): string {
+  return `${change >= 0 ? '+' : ''}${change.toFixed(2)}%`;
+}
